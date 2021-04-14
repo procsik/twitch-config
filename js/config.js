dc0a829e80715852e731ecb18fd2314f9edf3bce
unknown <procsik@gmail.com>
2021-04-14 17:27:36 +0300

    140421

--- js/config.js
+++ js/config.js
@@ -2,29 +2,49 @@ document.addEventListener("load", main())
 
 function main() {
     // init
+    let settings = new Object({
+        color: '',
+        role: '',
+        motiv: '',
+    })
+
     document.getElementsByName('color')[0].checked = true
+    settings.color = document.getElementsByName('color')[0].value
+
     document.getElementsByName('role')[1].checked = true
+    settings.role = document.getElementsByName('role')[1].value
+
     document.getElementsByName('motiv')[0].checked = true
+    settings.motiv = document.getElementsByName('motiv')[0].value
 
     let color = document.getElementsByClassName('color')
     for (let c of color) {
-        c.onclick = function() {
+        c.onmousedown = function() {
             c.lastElementChild.checked = true
+            settings.color = c.lastElementChild.value
         }
     }
 
     let role = document.getElementsByClassName('role')
     for (let r of role) {
-        r.onclick = function() {
+        r.onmousedown = function() {
             r.lastElementChild.checked = true
+            settings.role = r.lastElementChild.value
         }
     }
     
     let motiv = document.getElementsByClassName('motiv')
     for (let m of motiv) {
-        m.onclick = function() {
+        m.onmousedown = function() {
             m.lastElementChild.checked = true
+            settings.motiv = m.lastElementChild.value
         }
     }
-}
+    
+    document.getElementById('button').onclick = function() {
+        console.log(settings)
+    }
 
+    let char = document.getElementsByName('killer')
+    console.log(char)
+}
