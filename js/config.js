542e3253bb71e4a683898249a48e38dcb9d64d32
unknown <procsik@gmail.com>
2021-04-15 17:06:54 +0300

    data.json

--- js/config.js
+++ js/config.js
@@ -6,7 +6,31 @@ function main() {
         color: '',
         role: '',
         motiv: '',
+        char: ''
     })
+
+    // achievements 
+    for (let a in data.stats.onteh) {
+        let rowAchiv = document.createElement('div')
+        rowAchiv.className = 'achivments border'
+
+        let rowAchivBox = document.createElement('input')
+        rowAchivBox.type = 'checkbox'
+        rowAchivBox.name = 'achivments'
+        rowAchivBox.value = a
+        rowAchiv.hidden = true
+        rowAchiv.innerHTML = data.stats.onteh[a].name
+
+        rowAchiv.onmousedown = () => {
+            if (rowAchiv.lastElementChild.checked) rowAchiv.lastElementChild.checked = false
+            else rowAchiv.lastElementChild.checked = true
+        }
+
+        rowAchiv.appendChild(rowAchivBox)
+
+        document.getElementById('column-c').appendChild(rowAchiv)
+        //data.stats.onteh[a]
+    }
 
     document.getElementsByName('color')[0].checked = true
     settings.color = document.getElementsByName('color')[0].value
@@ -16,6 +40,12 @@ function main() {
 
     document.getElementsByName('motiv')[0].checked = true
     settings.motiv = document.getElementsByName('motiv')[0].value
+
+    for (let char of document.getElementsByName('char')) {
+        char.hidden = true
+    }
+
+    // main
 
     let color = document.getElementsByClassName('color')
     for (let c of color) {
@@ -32,6 +62,10 @@ function main() {
             settings.role = r.lastElementChild.value
         }
     }
+
+    document.getElementById('character').onmousedown = () => {
+        showChar(settings.role)
+    }
     
     let motiv = document.getElementsByClassName('motiv')
     for (let m of motiv) {
@@ -45,6 +79,41 @@ function main() {
         console.log(settings)
     }
 
-    let char = document.getElementsByName('killer')
-    console.log(char)
+    let char = document.getElementsByName('char')
+    for (let c of char) {
+        c.onmousedown = function() {
+            c.lastElementChild.checked = true
+            settings.char = c.lastElementChild.value
+        }
+    }
+
+    /* let achiv = document.getElementsByClassName('achivments')
+    for (let a of achiv) {
+        a.onmousedown = () => {
+            if (a.lastElementChild.checked) a.lastElementChild.checked = false
+            else a.lastElementChild.checked = true
+        }
+    } */
+}
+
+function showChar(c) {
+    switch(c) {
+        case 'killer': {
+            for (let char of document.getElementsByName('char')) {
+                char.lastElementChild.checked = false
+                if (char.lastElementChild.value[0] == 'K') char.hidden = false
+                else char.hidden = true
+            }
+            break
+        }
+        case 'camper': {
+            for (let char of document.getElementsByName('char')) {
+                char.lastElementChild.checked = false
+                if (char.lastElementChild.value[0] == 'C') char.hidden = false
+                else char.hidden = true
+            }
+            break
+        }
+        default: break
+    }
 }
---
+++ js/data.js
@@ -0,0 +1,93 @@
+let data =
+{
+    "config": {
+
+    },
+    "stats": {
+        "steam": {
+
+        },
+        "onteh": {
+            "bloodpoints":{
+                "position":239,
+                "value":13134,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/a884de3aafb36c5a2590c698164d4aa1baff3357.jpg",
+                "name":"Кровавый миллионер",
+                "desc":"Накопите в общей сложности {value} очков крови (их можно тратить до достижения общего количества)."
+            },
+            "survivor_perfectgames":{
+                "position":854,
+                "value":578,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/aebba5de55ab1a6d0d3404eab922656fdecd8516.jpg",
+                "name":"Идеальный побег",
+                "desc":"В публичном матче за выжившего, завершите матч, имея хотя бы по 5000 очков в каждой категории."
+            },
+            "gensrepaired":{
+                "position":69,
+                "value":29486,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/6c59e716ebf92641dfd0a7488549a36bb1249148.jpg",
+                "name":"Золотые руки",
+                "desc":"В публичных матчах почините в общей сложности {value} генераторов."
+            },
+            "survivorshealed":{
+                "position":416,
+                "value":8712,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/5f4bc7671b0d6fdf3739f9ee00a5ed1d0164a673.jpg",
+                "name":"Медик",
+                "desc":"В публичных матчах в общей сложности {value} раз вылечите других выживших."
+            },
+            "skillchecks":{
+                "position":115,
+                "value":119412,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/5505c2f2e868307eae06905c66a5bdf88a0a1c45.jpg",
+                "name":"Стальные нервы",
+                "desc":"В публичных матчах успешно пройдите {value} проверок реакции."
+            },
+            "saved":{
+                "position":776,
+                "value":12379,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/04740b83fb74fe8d72e3146a6960dc0c65dc2e44.jpg",
+                "name":"Я тебя прикрою!",
+                "desc":"В публичных матчах спасите {value} выживших от смерти."
+            },
+            "escaped":{
+                "position":42,
+                "value":9326,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/2b73698f686b275cf0913c30523e5c94a91eae28.jpg",
+                "name":"Выбрался живым!",
+                "desc":"В публичных матчах {value} раз сбегите живым."
+            },
+            "escaped_hatch":{
+                "position":561,
+                "value":729,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/2c52e2eded31f8a5037d141c804d2554db6a244c.jpg",
+                "name":"Побег через черный ход.",
+                "desc":"В публичном матче сбегите через люк."
+            },
+            "protectionhits":{
+                "position":8442,
+                "value":100,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/6c5e6e3a9566d5f3f2b47bf88f55900083aaadbe.jpg",
+                "name":"Телохранитель",
+                "desc":"В публичных матчах примите {value} защитных ударов на себя, когда убийца несет выжившего."
+            },
+            "obsessionshealed":{
+                "position":5019,
+                "value":235,
+                "id":0,
+                "imgUrl":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/381210/0c24b398295b7b33b4b87e977b80470dd63d1433.jpg",
+                "name":"Здоровая одержимость",
+                "desc":"В публичных матчах восстановите другим выжившим-объектам одержимости суммарно {value} состояний здоровья."
+            }
+        }
+    }
+}
