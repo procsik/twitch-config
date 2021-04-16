6951d5d92f0dd719cff3f7fb02a997bc5302d33c
unknown <procsik@gmail.com>
2021-04-16 16:18:40 +0300

    160142021

--- js/config.js
+++ js/config.js
@@ -6,30 +6,68 @@ function main() {
         color: '',
         role: '',
         motiv: '',
-        char: ''
+        char: '',
+        achiv: []
     })
+    let achivTmp = {}
 
     // achievements 
-    for (let a in data.stats.onteh) {
+    for (let a in sortByPos(data.stats.onteh)) {
         let rowAchiv = document.createElement('div')
         rowAchiv.className = 'achivments border'
+        rowAchiv.onclick = () => {
+            if (rowAchiv.lastElementChild.checked) {
+                rowAchiv.lastElementChild.checked = false
+                rowAchiv.style.background = ''
+
+                /* if (settings.achiv[rowAchiv.lastElementChild.value]) {
+                    delete settings.achiv[rowAchiv.lastElementChild.value]
+                } */
+
+                if (settings.achiv.includes(rowAchiv.lastElementChild.value) && achivTmp[rowAchiv.lastElementChild.value]) {
+                    settings.achiv.splice(settings.achiv.indexOf(rowAchiv.lastElementChild.value),1)
+                    delete achivTmp[rowAchiv.lastElementChild.value]
+                } 
+                settings.achiv = Object.keys(sortByPos(achivTmp))
+            }
+            else if (Object.keys(settings.achiv).length < 5) {
+                rowAchiv.lastElementChild.checked = true
+                rowAchiv.style.background = '#00ff00'
+                /* if (settings.achiv[rowAchiv.lastElementChild.value] === undefined) {
+                    settings.achiv[rowAchiv.lastElementChild.value] = data.stats.onteh[a]
+                }  */
+
+                if (!settings.achiv.includes(rowAchiv.lastElementChild.value)) {
+                    //settings.achiv.push(rowAchiv.lastElementChild.value)
+                    achivTmp[rowAchiv.lastElementChild.value] = data.stats.onteh[a]
+                }
+                settings.achiv = Object.keys(sortByPos(achivTmp))
+            }
+        }
+
+        let achivImg = document.createElement('div')
+        achivImg.className = 'achiv-img border'
+        achivImg.style.backgroundImage = 'url('+ data.stats.onteh[a].imgUrl +')'
+        rowAchiv.appendChild(achivImg)
+
+        let achivName = document.createElement('div')
+        achivName.className = 'achiv-name border'
+        achivName.innerHTML = data.stats.onteh[a].name
+        rowAchiv.appendChild(achivName)
+
+        let achivPos = document.createElement('div')
+        achivPos.className = 'achiv-pos border'
+        achivPos.innerHTML = data.stats.onteh[a].position
+        rowAchiv.appendChild(achivPos)
 
         let rowAchivBox = document.createElement('input')
         rowAchivBox.type = 'checkbox'
         rowAchivBox.name = 'achivments'
         rowAchivBox.value = a
-        rowAchiv.hidden = true
-        rowAchiv.innerHTML = data.stats.onteh[a].name
-
-        rowAchiv.onmousedown = () => {
-            if (rowAchiv.lastElementChild.checked) rowAchiv.lastElementChild.checked = false
-            else rowAchiv.lastElementChild.checked = true
-        }
-
+        rowAchivBox.hidden = true
         rowAchiv.appendChild(rowAchivBox)
 
         document.getElementById('column-c').appendChild(rowAchiv)
-        //data.stats.onteh[a]
     }
 
     document.getElementsByName('color')[0].checked = true
@@ -116,4 +154,15 @@ function showChar(c) {
         }
         default: break
     }
+}
+
+function sortByPos(obj) {
+    let newObj = new Object()
+    Object.keys(obj).sort(function(a,b){
+        return obj[a].position - obj[b].position
+    }).forEach(function(v){
+        /* console.log(obj[v].points.toFixed()) */
+        newObj[v] = obj[v]
+    })
+    return newObj
 }
