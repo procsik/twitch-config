28a8e4c73dbb9200d3241616d8e2cee08298befa
unknown <procsik@gmail.com>
2021-04-21 17:39:39 +0300

    2104

--- js/config.js
+++ js/config.js
@@ -7,16 +7,33 @@ function main() {
         role: '',
         motiv: '',
         char: '',
-        achiv: ["bloodpoints","survivorshealed","escaped_hatch","saved"]
+        // achivMain: ["escaped","protectionhits"],
+        // achiv: ["bloodpoints","survivorshealed","escaped_hatch","saved"]
+        achiv: [
+            ["escaped","protectionhits"],
+            ["bloodpoints","survivorshealed","escaped_hatch","saved"]
+        ]
     })
     let cSetup = ''
+    // let achivMainTmp = {}
     let achivTmp = {}
-}
+    
+    // Object.keys(settings.achivMain)
+    //     .forEach(key => achivMainTmp[settings.achivMain[key]] = data.stats.onteh[settings.achivMain[key]])
+    // Object.keys(settings.achiv)
+    //     .forEach(key => achivTmp[settings.achiv[key]] = data.stats.onteh[settings.achiv[key]])
+    Object.keys(settings.achiv)
+        .forEach(key => {
+            achivTmp[key] = {}
+            settings.achiv[key].forEach(k => achivTmp[key][k] = data.stats.onteh[k])
+        })
+
+
     // achievements 
     for (let a in sortByPos(data.stats.onteh)) {
         let rowAchiv = document.createElement('div')
         rowAchiv.className = 'achivments border'
-        rowAchiv.onclick = () => changeAchiv(rowAchiv,a)
+        rowAchiv.onclick = () => changeAchiv(rowAchiv,a,cSetup)
         rowAchiv.style = 'display: none'
 
         let achivImg = document.createElement('div')
@@ -81,6 +98,7 @@ function main() {
     let color = document.getElementsByClassName('color')
     for (let c of color) {
         c.onmousedown = function() {
+            cSetup = 'color'
             c.lastElementChild.checked = true
             settings.color = c.lastElementChild.value
         }
@@ -89,6 +107,7 @@ function main() {
     let role = document.getElementsByClassName('role')
     for (let r of role) {
         r.onmousedown = () => {
+            cSetup = 'role'
             r.lastElementChild.checked = true
             settings.role = r.lastElementChild.value
         }
@@ -108,6 +127,7 @@ function main() {
     let motiv = document.getElementsByClassName('motiv')
     for (let m of motiv) {
         m.onmousedown = function() {
+            cSetup = 'motiv'
             m.lastElementChild.checked = true
             settings.motiv = m.lastElementChild.value
         }
@@ -132,43 +152,78 @@ function main() {
             else a.lastElementChild.checked = true
         }
     } */
-    function changeAchiv(elem,a) {
-        if (elem.lastElementChild.checked) {
-            elem.lastElementChild.checked = false
-            elem.style.background = ''
-    
-            /* if (settings.achiv[rowAchiv.lastElementChild.value]) {
-                delete settings.achiv[rowAchiv.lastElementChild.value]
-            } */
-    
-            if (settings.achiv.includes(elem.lastElementChild.value) && achivTmp[elem.lastElementChild.value]) {
-                settings.achiv.splice(settings.achiv.indexOf(elem.lastElementChild.value),1)
-                delete achivTmp[elem.lastElementChild.value]
-            } 
-            settings.achiv = Object.keys(sortByPos(achivTmp))
-        }
-        else if (Object.keys(settings.achiv).length < 5) {
-            elem.lastElementChild.checked = true
-            elem.style.background = '#00ff00'
-            /* if (settings.achiv[rowAchiv.lastElementChild.value] === undefined) {
-                settings.achiv[rowAchiv.lastElementChild.value] = data.stats.onteh[a]
-            }  */
-    
-            if (!settings.achiv.includes(elem.lastElementChild.value)) {
-                //settings.achiv.push(rowAchiv.lastElementChild.value)
-                achivTmp[elem.lastElementChild.value] = data.stats.onteh[a]
+    function changeAchiv(elem,a,s) {
+        switch(s) {
+            case "achivMain": {
+                if (elem.lastElementChild.checked) {
+                    if (settings.achiv[0].includes(elem.lastElementChild.value) && achivTmp[0][elem.lastElementChild.value]) {
+                        settings.achiv[0].splice(settings.achiv[0].indexOf(elem.lastElementChild.value),1)
+                        delete achivTmp[0][elem.lastElementChild.value]
+
+                        elem.lastElementChild.checked = false
+                        elem.style.background = ''
+                    } else if (settings.achiv[1].includes(elem.lastElementChild.value) && achivTmp[1][elem.lastElementChild.value] && (Object.keys(settings.achiv[0]).length < 2)) {
+                        settings.achiv[1].splice(settings.achiv[1].indexOf(elem.lastElementChild.value),1)
+                        delete achivTmp[1][elem.lastElementChild.value]
+
+                        achivTmp[0][elem.lastElementChild.value] = data.stats.onteh[a]
+
+                        elem.lastElementChild.checked = true
+                        elem.style.background = '#00FF00'
+                    }
+
+                    settings.achiv[0] = Object.keys(sortByPos(achivTmp[0]))
+                    settings.achiv[1] = Object.keys(sortByPos(achivTmp[1]))
+                } else if ((Object.keys(settings.achiv[0]).length < 2) && !elem.lastElementChild.checked) {
+                    elem.lastElementChild.checked = true
+                    elem.style.background = '#00ff00'
+
+                    if (!settings.achiv[0].includes(elem.lastElementChild.value)) {
+                        achivTmp[0][elem.lastElementChild.value] = data.stats.onteh[a]
+                    }
+
+                    settings.achiv[0] = Object.keys(sortByPos(achivTmp[0]))
+                }
+                break
             }
-            settings.achiv = Object.keys(sortByPos(achivTmp))
+            case "achiv": {
+                if (elem.lastElementChild.checked) {
+                    if (settings.achiv[1].includes(elem.lastElementChild.value) && achivTmp[1][elem.lastElementChild.value]) {
+                        settings.achiv[1].splice(settings.achiv[1].indexOf(elem.lastElementChild.value),1)
+                        delete achivTmp[1][elem.lastElementChild.value]
+
+                        elem.lastElementChild.checked = false
+                        elem.style.background = ''
+                    }
+
+                    settings.achiv[1] = Object.keys(sortByPos(achivTmp[1]))
+                } else if (Object.keys(settings.achiv[1]).length < 5) {
+                    elem.lastElementChild.checked = true
+                    elem.style.background = '#00FF00'
+
+                    if (!settings.achiv[1].includes(elem.lastElementChild.value)) {
+                        achivTmp[1][elem.lastElementChild.value] = data.stats.onteh[a]
+                    }
+
+                    settings.achiv[1] = Object.keys(sortByPos(achivTmp[1]))
+                }
+                break
+            }
+            default: break
         }
     }
 
     function unhide(a) {
         switch(a) {
             case "main-achiv": {
+                cSetup = 'achivMain'
                 for (let c of document.getElementById('column-c').children) {
                     if (Object.keys(c.classList).map(key => c.classList[key]).includes('achivments')) {
-                        if (settings.achiv.includes(c.lastElementChild.value)) {
+                        if (settings.achiv[0].includes(c.lastElementChild.value)) {
                             c.style.background = '#00FF00'
+                            c.lastElementChild.checked = true
+                        } else if (settings.achiv[1].includes(c.lastElementChild.value)) {
+                            c.style.background = '#0000FF'
                             c.lastElementChild.checked = true
                         } else {
                             c.style.background = ''
@@ -180,13 +235,25 @@ function main() {
                 break
             }
             case "achiv": {
-                console.log(a)
+                cSetup = 'achiv'
                 for (let c of document.getElementById('column-c').children) {
-                    c.style = "display:"
+                    if (Object.keys(c.classList).map(key => c.classList[key]).includes('achivments')) {
+                        if (settings.achiv[0].includes(c.lastElementChild.value)) {
+                            c.style.background = '#808080'
+                        } else if (settings.achiv[1].includes(c.lastElementChild.value)) {
+                            c.style.background = '#00FF00'
+                            c.lastElementChild.checked = true
+                        } else {
+                            c.style.background = ''
+                        }
+                        c.style.display = ''
+                    }
+                    else c.style = "display: none"
                 }
                 break
             }
             case "char": {
+                cSetup = 'char'
                 for (let c of document.getElementById('column-c').children) {
                     if (Object.keys(c.classList).map(key => c.classList[key]).includes('char')) {
                         c.style = "display:"
